523e7a2d5075b991b5823e397f4a27febed7f62e
Rume7 <electengrhume@gmail.com>
2023-07-15 17:28:34 +0100

    Wrote test cases for Capulet engine.

--- tests/enginetest/EngineTest.py
+++ tests/enginetest/EngineTest.py
@@ -1,23 +1,39 @@
 import unittest
 from datetime import datetime
 
-from car.Calliope import Calliope
+from engine.CapuletEngine import CapuletEngine
 
 
 class TestEngine(unittest.TestCase):
     
-    def test_engine_should_be_serviced(self):
-        last_service_date = datetime.today().date()
-        current_mileage = 30001
-        last_service_mileage = 0
+    def test_capulet_engine_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_year = today.year - 4
+        last_service_date = today.replace(last_service_year, today.month, today.day)
+        current_mileage = 40200
+        last_service_mileage = 10000
 
-        car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        self.assertTrue(car.needs_service())
+        capulet_engine = CapuletEngine(last_service_date, last_service_mileage, current_mileage)
+        self.assertTrue(capulet_engine.needs_service())
 
-    def test_engine_should_not_be_serviced(self):
-        last_service_date = datetime.today().date()
-        current_mileage = 30000
-        last_service_mileage = 0
+    def test_capulet_engine_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_year = today.year - 4
+        last_service_date = today.replace(last_service_year, today.month, today.day)
+        current_mileage = 40200
+        last_service_mileage = 23000
 
-        car = Calliope(last_service_date, current_mileage, last_service_mileage)
-        self.assertFalse(car.needs_service())
+        capulet_engine = CapuletEngine(last_service_date, last_service_mileage, current_mileage)
+        self.assertFalse(capulet_engine.needs_service())
+
+    def test_capulet_engine_negative_mileage_difference(self):
+        today = datetime.today().date()
+        last_service_year = today.year - 4
+        last_service_date = today.replace(last_service_year, today.month, today.day)
+        current_mileage = 40200
+        last_service_mileage = 43000
+
+        capulet_engine = CapuletEngine(last_service_date, last_service_mileage, current_mileage)
+
+        with self.assertRaises(ValueError):
+            capulet_engine.needs_service()
