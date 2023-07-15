a54bc191e1e4c4a21837aa0ad4216c48b1209f71
Rume7 <electengrhume@gmail.com>
2023-07-15 20:26:35 +0100

    updated create car method.

--- factory/CarFactory.py
+++ factory/CarFactory.py
@@ -1,34 +1,28 @@
-from car.Calliope import Calliope
-from car.Car import Car
-from car.Glissade import Glissade
-from car.Palindrome import Palindrome
-from car.Rorschach import Rorschach
-from car.Thovex import Thovex
+from car import Car, Calliope, Glissade, Palindrome, Rorschach, Thovex
 from ICarFactory import ICarFactory
-from factory.BatteryFactory import BatteryFactory
-from factory.EngineFactory import EngineFactory
+from factory import TireFactory, EngineFactory, BatteryFactory
 
 
 class CarFactory(ICarFactory):
-    
+
     @staticmethod
-    def create_a_car(car_type, engine_type, battery_type, last_service_date, last_service_mileage, 
-                        current_mileage, current_date, warning_light_on) -> Car :
-        
+    def create_a_car(car_type, engine_type, battery_type, last_service_date, last_service_mileage,
+                     current_mileage, current_date, warning_light_on,
+                     tire_type, sensor_array) -> Car:
+
         an_engine = EngineFactory.create_engine(engine_type, last_service_mileage, current_mileage, warning_light_on)
         a_battery = BatteryFactory.create_battery(battery_type, last_service_date, current_date)
-            
-        if (car_type.capitalize() == 'Calliope'):
-            return Calliope(an_engine, a_battery, last_service_date)
-        elif (car_type.capitalize() == 'Glissade'):
-            return Glissade(an_engine, a_battery, last_service_date)
-        elif (car_type.capitalize() == 'Palindrome'):
-            return Palindrome(an_engine, a_battery, last_service_date)
-        elif (car_type.capitalize() == 'Rorschach'):
-            return Rorschach(an_engine, a_battery, last_service_date)
-        elif (car_type.capitalize() == 'Thovex'):
-            return Thovex(an_engine, a_battery, last_service_date)
-        else: 
+        a_tyre = TireFactory.create_tire(tire_type, sensor_array)
+
+        if car_type.capitalize() == 'Calliope':
+            return Calliope(an_engine, a_battery, a_tyre, last_service_date)
+        elif car_type.capitalize() == 'Glissade':
+            return Glissade(an_engine, a_battery, a_tyre, last_service_date)
+        elif car_type.capitalize() == 'Palindrome':
+            return Palindrome(an_engine, a_battery, a_tyre, last_service_date)
+        elif car_type.capitalize() == 'Rorschach':
+            return Rorschach(an_engine, a_battery, a_tyre, last_service_date)
+        elif car_type.capitalize() == 'Thovex':
+            return Thovex(an_engine, a_battery, a_tyre, last_service_date)
+        else:
             raise ValueError("Wrong type of car.")
-        
-        
