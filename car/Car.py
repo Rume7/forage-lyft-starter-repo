33fa41fc15f0baacc5af56199a58b3b73c8e2eb0
Rume7 <electengrhume@gmail.com>
2023-07-15 20:20:32 +0100

    Added the tyre instance variable.

--- car/Car.py
+++ car/Car.py
@@ -2,22 +2,21 @@ from car.Serviceable import Serviceable
 
 
 class Car(Serviceable):
-    
-    def __init__(self, engine, battery, last_service_date):
+
+    def __init__(self, engine, battery, tyre, last_service_date):
         self.engine = engine
         self.battery = battery
+        self.tyre = tyre
         self.last_service_date = last_service_date
-    
-    
+
     def get_engine(self):
         return self.engine
-        
-        
+
     def get_battery(self):
         return self.battery
-            
-      
+
+    def get_tyre(self):
+        return self.tyre
+
     def needs_service(self):
         return self.engine.needs_service() and self.battery.needs_service()
-          
-        
