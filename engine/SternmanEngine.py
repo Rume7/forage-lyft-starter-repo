45996a8b332393912b305dbddd53a5a0c0e6107c
Rume7 <electengrhume@gmail.com>
2023-07-15 17:52:35 +0100

    Refactored the Engine classes to handle value errors.

--- engine/SternmanEngine.py
+++ engine/SternmanEngine.py
@@ -2,11 +2,12 @@ from engine.IEngine import IEngine
 
 
 class SternmanEngine(IEngine):
-    
-    def __init__(self, warning_light_on):
-        self.warning_light_on = warning_light_on
-        
-        
-    def needs_service(self):
-        return self.warning_light_on == True
 
+    def __init__(self, last_service_date, warning_light_on):
+        super().__init__(last_service_date)
+        self.warning_light_on = warning_light_on
+
+    def needs_service(self):
+        if type(self.warning_light_on) != bool:
+            raise ValueError('Warning light on should be a boolean')
+        return self.warning_light_on
